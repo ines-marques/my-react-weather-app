1547f81f16452d24cd4d4030545a36586f705556
ines-marques <81305906+ines-marques@users.noreply.github.com>
2021-05-14 14:56:04 +0100

    Added GitHub link

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ export default function App() {
   return (
     <div className="App">
       Weather App
+      <footer>This project was coded by Inês Marques and is {" "}
+        <a href="https://github.com/ines-marques/my-react-weather-app" target="_blank" rel="noreferrer">open-sourced on GitHub</a>
+      </footer>
     </div>
   );
 }
-
