99a8022baa53cfc5d1224f14a088d3cdba70154f
ines-marques <81305906+ines-marques@users.noreply.github.com>
2021-05-14 18:22:09 +0100

    Weather API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import React from "react";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
+        <Weather />
         <footer>This project was coded by Inês Marques and is {" "}
         <a href="https://github.com/ines-marques/my-react-weather-app" target="_blank" rel="noreferrer">open-sourced on GitHub</a>
         </footer>
