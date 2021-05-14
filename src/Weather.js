ea5f72bbb5fb6184a9c36e65d4f950c1d9a945fd
ines-marques <81305906+ines-marques@users.noreply.github.com>
2021-05-14 18:42:57 +0100

    updating API

--- src/Weather.js
+++ src/Weather.js
@@ -2,8 +2,6 @@ import React, { useState } from "react";
 import "./Weather.css";
 import axios from "axios";
 
-//DATA!
-
 export default function Weather(props) {
     const [weatherData, setWeatherData] = useState({ ready: false });
     const [city, setCity] = useState(props.defaultCity);
@@ -22,7 +20,7 @@ export default function Weather(props) {
 
     function search() {
         const apiKey = "178be0a79146aa22863d056738d4f9b4";
-        let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=porto&appid=${apiKey}&units=metric`
+        let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
         axios.get(apiUrl).then(handleResponse);
     }
     
@@ -36,38 +34,44 @@ export default function Weather(props) {
         setCity(event.target.value);
     }
 
-    return (
-        <div className="Weather">
-            <div className="row">
-                <div className="col-6">
-                    <img src="https://openweathermap.org/img/wn/01d@2x.png" alt="" width="62"/>
-                    <span>16</span>
-                    <span>ºC</span>
-                    <span>ºF</span>
-                    <ul className="List">
-                        <li>Precipitation: {" "}<span></span>%</li>
-                        <li>Wind: {" "}<span></span>km/h</li>
-                    </ul>
-                </div>
-                <div className="col-6">
-                    <ul className="List Right">
-                        <li>City</li>
-                        <li>Date</li>
-                        <li>Description</li>
-                    </ul>
-                </div>
-            </div>
-            <form className="MyForm" onSubmit={handleSubmit}>
+    if (weatherData.ready) {
+        return (
+            <div className="Weather">
                 <div className="row">
-                    <div className="col-10">
-                        <input type="search" placeholder="Enter a city" className="form-control" autofocus="on" onChange={handleCityChange}/>
+                    <div className="col-6">
+                        <img src="https://openweathermap.org/img/wn/01d@2x.png" alt="" width="62"/>
+                        <span>16</span>
+                        <span>ºC</span>
+                        <span>ºF</span>
+                        <ul className="List">
+                            <li>Precipitation: {" "}<span></span>%</li>
+                            <li>Wind: {" "}<span></span>km/h</li>
+                        </ul>
                     </div>
-                    <div className="col-2">
-                        <input type="submit" value="Search" className="btn btn-primary w-100"/>
+                    <div className="col-6">
+                        <ul className="List Right">
+                            <li>City</li>
+                            <li>Date</li>
+                            <li>Description</li>
+                        </ul>
                     </div>
                 </div>
-            </form>
-        </div>
-    )
+                <form className="MyForm" onSubmit={handleSubmit}>
+                    <div className="row">
+                        <div className="col-10">
+                            <input type="search" placeholder="Enter a city" className="form-control" autofocus="on" onChange={handleCityChange}/>
+                        </div>
+                        <div className="col-2">
+                            <input type="submit" value="Search" className="btn btn-primary w-100"/>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        );
+    } else {
+        search();
+        return "Loading..."
+    }
+
 }                                            
 
