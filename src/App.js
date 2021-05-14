d0b6336f8ffd855f43c440bad78f36fa09e9a2fe
ines-marques <81305906+ines-marques@users.noreply.github.com>
2021-05-14 15:25:09 +0100

    added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import React from "react";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>This project was coded by Inês Marques and is {" "}
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>This project was coded by Inês Marques and is {" "}
         <a href="https://github.com/ines-marques/my-react-weather-app" target="_blank" rel="noreferrer">open-sourced on GitHub</a>
-      </footer>
+        </footer>
+      </div>
     </div>
   );
 }
